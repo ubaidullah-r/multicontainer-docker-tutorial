f6bbdcdb68ce75d17cfe7ac4212d5747448a1802
ubaid ullah <38988469+ubaidullah-r@users.noreply.github.com>
2023-04-07 06:36:52 +0500

    Update app.js

--- ui/src/app.js
+++ ui/src/app.js
@@ -5,13 +5,9 @@ var dist = path.resolve(__dirname + '/../dist/client');
 var fallbackPath = path.resolve(dist + '/index.html');
 var port = 4200;
 var datetime = "LastSync: " + new Date();
-
 app.use(express.static(dist));
-
 app.get('*', function(req, res) {
     res.sendFile(fallbackPath);
 });
-
 var lastsync = datetime + ': Dashboard is on port ' + port + '!'
-
 app.listen(port, () => console.log(lastsync));
